21a5116295a1e81dea67ae9db95951b3d9e0bca5
Dionatan <dionatan.oliveira@bttis.com>
2024-06-23 17:54:59 -0300

    iniciando aplicacao next

---
+++ web/src/components/EventCard.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { EventModel } from "../models";
+import Image from "next/image";
+
+export type EventCardProps = {
+  event: EventModel;
+};
+
+export function EventCard({event}: EventCardProps) {
+  return (
+    <Link href={`/event/${event.id}/spots-layout`}>
+      <div className="flex w-[277px] flex-col rounded-2xl bg-secondary">
+        <img src={event.image_url} alt={event.name} />
+        <div className="flex flex-col gap-y-2 px-4 py-6">
+          <p className="text-sm uppercase text-subtitle">
+            {new Date(event.date).toLocaleDateString("pt-BR", {
+              weekday: "long",
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+            })}
+          </p>
+          <p className="font-semibold">{event.name}</p>
+          <p className="text-sm font-normal">{event.location}</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
